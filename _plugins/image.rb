6f2e5fd3b301804e2e309cde09a6a082a7b7f4bc
Teo Huey Yun <thy231@nyu.edu>
2015-11-15 10:46:38 -0500

    add

--- _plugins/image.rb
+++ _plugins/image.rb
@@ -1,50 +1,50 @@
-module Jekyll
-  class ImageTag < Liquid::Tag
-    @url = nil
-    @caption = nil
-    @class = nil
+# Title: Simple Image tag for Jekyll
+# Authors: Brandon Mathis http://brandonmathis.com
+#          Felix Schäfer, Frederic Hemberger
+# Description: Easily output images with optional class names, width, height, title and alt attributes
+#
+# Syntax {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | "title text" ["alt text"]] %}
+#
+# Examples:
+# {% img /images/ninja.png Ninja Attack! %}
+# {% img left half http://site.com/images/ninja.png Ninja Attack! %}
+# {% img left half http://site.com/images/ninja.png 150 150 "Ninja Attack!" "Ninja in attack posture" %}
+#
+# Output:
+# <img src="/images/ninja.png">
+# <img class="left half" src="http://site.com/images/ninja.png" title="Ninja Attack!" alt="Ninja Attack!">
+# <img class="left half" src="http://site.com/images/ninja.png" width="150" height="150" title="Ninja Attack!" alt="Ninja in attack posture">
+#
 
-    IMAGE_URL_WITH_CLASS_AND_CAPTION = /(\w+)(\s+)([^\s]+)(\s+)"(.*?)"/i
-    IMAGE_URL_WITH_CAPTION = /([^\s]+)(\s+)"(.*?)"/i
-    IMAGE_URL_WITH_CLASS = /(\w+)(\s+)([^\s]+)/i
-    IMAGE_URL = /([^\s]+)/i
+module Jekyll
+
+  class ImageTag < Liquid::Tag
+    @img = nil
 
     def initialize(tag_name, markup, tokens)
-      super
+      attributes = ['class', 'src', 'width', 'height', 'title']
 
-      if markup =~ IMAGE_URL_WITH_CLASS_AND_CAPTION
-        @class   = $1
-        @url     = $3
-        @caption = $5
-      elsif markup =~ IMAGE_URL_WITH_CAPTION
-        @url     = $1
-        @caption = $3
-      elsif markup =~ IMAGE_URL_WITH_CLASS
-        @class = $1
-        @url   = $3
-      elsif markup =~ IMAGE_URL
-        @url = $1
+      if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
+        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
+        if /(?:"|')(?<title>[^"']+)?(?:"|')\s+(?:"|')(?<alt>[^"']+)?(?:"|')/ =~ @img['title']
+          @img['title']  = title
+          @img['alt']    = alt
+        else
+          @img['alt']    = @img['title'].gsub!(/"/, '&#34;') if @img['title']
+        end
+        @img['class'].gsub!(/"/, '') if @img['class']
       end
-
+      super
     end
 
     def render(context)
-      source = "<figure class='#{@class}'>"
-
-      source += "<a href=\"/images/#{@url}\">"
-
-      source += "<img src=\"/images/#{@url}\">"
-
-      source += "</a>"
-
-      @caption = Kramdown::Document.new(@caption).to_html if @caption
-      source += "<figcaption>#{@caption}</figcaption>" if @caption
-
-      source += "</figure>"
-
-      source
+      if @img
+        "<img #{@img.collect {|k,v| "#{k}=\"#{v}\"" if v}.join(" ")}>"
+      else
+        "Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \"title text\" [\"alt text\"]] %}"
+      end
     end
   end
 end
 
-Liquid::Template.register_tag('image', Jekyll::ImageTag)
+Liquid::Template.register_tag('img', Jekyll::ImageTag)
