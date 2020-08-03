93a0a34650583ac40b6d5cb017bd4489ccb93bf0
Fivos Avgerinos <afivos@me.com>
2020-08-03 17:21:36 +0300

    Bump

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,31 +1,36 @@
-// Google Analytics
+const nightSwitch = document.getElementById("darkMode");
+let mode = localStorage.getItem("mode");
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+const applyMode = (mode) => {
+    document.documentElement.setAttribute("user-color-mode", mode);
+    // const text = mode === "night" ? "&#x2600;" : "&#x25CF;";
+    // nightSwitch.innerHTML = text;
+}
 
-ga('create', 'UA-48466891-1', 'auto');
-ga('send', 'pageview');
+applyMode(mode);
 
-
-// Fluidbox
-
-$(function () {
-    $('a').fluidbox({
-	    immediateOpen: true,
-	    loader: true
-    });
-})
-
-// Close instance after viewport scroll
-$(window).scroll(function() {
-	$('.fluidbox--opened').fluidbox('close');
+nightSwitch.addEventListener("click", () => {
+    mode = mode === "day" ? "night" : "day";
+    localStorage.setItem("mode", mode);
+    applyMode(mode);
 });
 
-// Close instance after 'esc' key is pressed
-$(window).bind('keydown', function(e) {
-    if (e.which == 27) {
-        $('.fluidbox--opened').fluidbox('close');
+// Initialize library
+/*eslint no-undef: "error"*/
+/*global lozad, lozad*/
+lozad('.lozad', {
+    load: function(el) {
+        el.src = el.dataset.src;
+        el.onload = function() {
+            el.classList.add('fade')
+        }
     }
-});
+}).observe()
+
+function showMore() {
+    var x = document.getElementById("hiddenBioText");
+    var y = document.getElementById("hiddenBioButton");
+
+    x.classList.add('active');
+    y.style.display = "none";
+}
--- readme.md
+++
@@ -1,4 +0,0 @@
-### Welcome
-This is [my website](http://afivos.com). Styled with [Sass](http://sass-lang.com), generated in [Jekyll](http://jekyllrb.com). Nothing fancy about it.
-
-I decided to open source it for those who are interested in that kind of thing. Who knows? My code can hopefully help you learn something new.
