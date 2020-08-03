4ab44e12325802211bf1b601df46c7507dbda664
Fivos Avgerinos <afivos@me.com>
2020-08-03 17:55:51 +0300

    Remove lozal.js (lazy load)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,18 +15,6 @@ nightSwitch.addEventListener("click", () => {
     applyMode(mode);
 });
 
-// Initialize library
-/*eslint no-undef: "error"*/
-/*global lozad, lozad*/
-lozad('.lozad', {
-    load: function(el) {
-        el.src = el.dataset.src;
-        el.onload = function() {
-            el.classList.add('fade')
-        }
-    }
-}).observe()
-
 function showMore() {
     var x = document.getElementById("hiddenBioText");
     var y = document.getElementById("hiddenBioButton");
