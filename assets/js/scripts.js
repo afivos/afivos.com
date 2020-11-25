ae99e6824c35e191dff36fef3e516d3dd72fbc02
Fivos Avgerinos <afivos@me.com>
2020-11-25 16:45:55 +0000

    Style active nav link

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -22,3 +22,12 @@ function showMore() {
     x.classList.add('active');
     y.style.display = "none";
 }
+
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll('a');
+const menuLength = menuItem.length
+for (let i = 0; i<menuLength; i++){
+    if(menuItem[i].href === currentLocation){
+        menuItem[i].className = "active"
+    }
+}
